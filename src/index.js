75bd6db9d59e3c5a4b7afbc6b64096e91e3b27f5
Jan Krupinski <jk.krupinski@gmail.com>
2021-02-24 22:36:09 +0100

    Add my songs controller

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("setup project");
+const lyricsAngularJS = angular.module('lyrics-angularjs', []);
+
+lyricsAngularJS.controller('my-songs', function mySongsController($scope) {
+  $scope.bands = JSON.parse(localStorage.getItem('bands') || '') || [];
+
+  $scope.deleteBand = id => {
+    $scope.bands = $scope.bands.filter(band => band.id !== id);
+
+    localStorage.setItem('bands', JSON.stringify($scope.bands));
+  }
+
+  $scope.deleteSong = (bandId, id) => {
+    const currBand = $scope.bands.find(band => band.id === bandId);
+    currBand.songs = currBand.songs.filter(song => song.id !== id);
+
+    if(currBand.songs.length === 0) {
+      $scope.bands = $scope.bands.filter(band => band.id !== bandId);
+    }
+    else {
+      $scope.bands = $scope.bands.map(band => {
+        if(band.id === bandId) return currBand;
+
+        return band;
+      })
+    }
+
+    localStorage.setItem('bands', JSON.stringify($scope.bands));
+  }
+});

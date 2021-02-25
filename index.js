07cf90e9524df280c7906b70b79493e96771483a
Jan Krupinski <jk.krupinski@gmail.com>
2021-02-25 22:18:28 +0100

    Add navbar

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require('./config');
 
 const app = express();
 app.use(express.static('src'));
+app.use(express.static('node_modules'));
 
 app.get('/', (req, res) => {
   res.sendFile('index.html' , { root : __dirname})

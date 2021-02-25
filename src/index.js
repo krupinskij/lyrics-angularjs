c7dc494516e688961aa93b64b090d5d2df08e326
Jan Krupinski <jk.krupinski@gmail.com>
2021-02-25 22:49:17 +0100

    Add home page

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
-const lyricsAngularJS = angular.module('lyrics-angularjs', []);
-
-lyricsAngularJS.controller('my-songs', function mySongsController($scope) {
-  $scope.bands = JSON.parse(localStorage.getItem('bands') || '') || [];
-
-  $scope.deleteBand = id => {
-    $scope.bands = $scope.bands.filter(band => band.id !== id);
-
-    localStorage.setItem('bands', JSON.stringify($scope.bands));
+angular.module('app', [
+  'ngRoute'
+])
+.config([
+  '$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+      .when('/', { templateUrl: 'views/home-page.html' })
+      .when('/aaa', { templateUrl: 'views/home-page.html' })
   }
-
-  $scope.deleteSong = (bandId, id) => {
-    const currBand = $scope.bands.find(band => band.id === bandId);
-    currBand.songs = currBand.songs.filter(song => song.id !== id);
-
-    if(currBand.songs.length === 0) {
-      $scope.bands = $scope.bands.filter(band => band.id !== bandId);
-    }
-    else {
-      $scope.bands = $scope.bands.map(band => {
-        if(band.id === bandId) return currBand;
-
-        return band;
-      })
-    }
-
-    localStorage.setItem('bands', JSON.stringify($scope.bands));
-  }
-});
+]);

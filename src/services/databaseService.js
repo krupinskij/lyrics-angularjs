cc5b1aaa940803105e0cdbe1e2998d270effee9c
Jan Krupinski <jk.krupinski@gmail.com>
2021-03-01 18:00:47 +0100

    Use angular promises

--- src/services/databaseService.js
+++ src/services/databaseService.js
@@ -1,169 +1,127 @@
-app.factory('$database', function() {
+app.factory('$database', function($q) {
   const $database = {};
   const DB_NAME = 'lyricsDB';
   const DB_VERSION = 1;
 
+  prepareDatabase = () => {
+    const deferred = $q.defer();
+
+    if(!indexedDB) {
+      deferred.reject("Your browser doesn't support IndexedDB");
+    }
+
+    const dbPromise = indexedDB.open(DB_NAME, DB_VERSION);
+
+    dbPromise.onupgradeneeded = event => {
+      const database = event.target.result;
+      if(!database.objectStoreNames.contains('song')) {
+        database
+          .createObjectStore('song', { keyPath: 'id', autoIncrement: true })
+          .createIndex('band', 'band', { unique: false })
+      }
+    }
+
+    dbPromise.onerror = () => {
+      deferred.reject("Connecting to database failed");
+    }
+
+    return {
+      deferred,
+      dbPromise
+    }
+  }
+
   $database.save = song => {
-    return new Promise(function(resolve, reject) {
-      if(!indexedDB) {
-        reject("Your browser doesn't support IndexedDB");
-      }
+    const { deferred, dbPromise } = prepareDatabase();
+    
+    dbPromise.onsuccess = event => {
+      const database = event.target.result;
+      const os = database.transaction('song', 'readwrite').objectStore('song');
 
-      const dbPromise = indexedDB.open(DB_NAME, DB_VERSION);
-
-      dbPromise.onupgradeneeded = event => {
-        const database = event.target.result;
-        if(!database.objectStoreNames.contains('song')) {
-          database
-            .createObjectStore('song', { keyPath: 'id', autoIncrement: true })
-            .createIndex('band', 'band', { unique: false })
-        }
+      const request = os.add(song);
+      request.onsuccess = () => {
+        deferred.resolve("New song added");
       }
-
-      dbPromise.onsuccess = event => {
-        const database = event.target.result;
-        const os = database.transaction('song', 'readwrite').objectStore('song');
-  
-        const request = os.add(song);
-        request.onsuccess = () => {
-          resolve("New song added");
-        }
-        request.onerror = () => {
-          reject("Adding new object failed");
-        }
+      request.onerror = () => {
+        deferred.reject("Adding new object failed");
       }
+    }
 
-      dbPromise.onerror = () => {
-        reject("Connecting to database failed");
-      }
-    })
+    return deferred.promise;
   }
 
   $database.getAll = () => {
-    return new Promise(function(resolve, reject) {
-      if(!indexedDB) {
-        reject("Your browser doesn't support IndexedDB");
-      }
+    const { deferred, dbPromise } = prepareDatabase();
 
-      const dbPromise = indexedDB.open(DB_NAME, DB_VERSION);
-
-      dbPromise.onupgradeneeded = event => {
-        const database = event.target.result;
-        if(!database.objectStoreNames.contains('song')) {
-          database
-            .createObjectStore('song', { keyPath: 'id', autoIncrement: true })
-            .createIndex('band', 'band', { unique: false })
-        }
-      }
-
-      dbPromise.onsuccess = event => {
-        const database = event.target.result;
-        const os = database.transaction('song', 'readwrite').objectStore('song');
+    dbPromise.onsuccess = event => {
+      const database = event.target.result;
+      const os = database.transaction('song', 'readwrite').objectStore('song');
         
-        const request = os.getAll();
-        request.onsuccess = event => {
-          resolve(event.target.result);
-        }
-        request.onerror = () => {
-          reject("Error retrieving data");
-        }
-        
+      const request = os.getAll();
+      request.onsuccess = event => {
+        deferred.resolve(event.target.result);
       }
+      request.onerror = () => {
+        deferred.reject("Error retrieving data");
+      }
+    }
 
-      dbPromise.onerror = () => {
-        reject("Connecting to database failed");
-      }
-    })
+    return deferred.promise;
   }
 
   $database.getByBandName = bandName => {
-    return new Promise(function(resolve, reject) {
-      if(!indexedDB) {
-        reject("Your browser doesn't support IndexedDB");
-      }
+    const { deferred, dbPromise } = prepareDatabase();
 
-      const dbPromise = indexedDB.open(DB_NAME, DB_VERSION);
-
-      dbPromise.onupgradeneeded = event => {
-        const database = event.target.result;
-        if(!database.objectStoreNames.contains('song')) {
-          database
-            .createObjectStore('song', { keyPath: 'id', autoIncrement: true })
-            .createIndex('band', 'band', { unique: false })
+    dbPromise.onsuccess = event => {
+      const database = event.target.result;
+      const os = database.transaction('song', 'readwrite').objectStore('song');
+      const oc = os.index('band').openCursor(bandName);
+      const data = [];
+      
+      oc.onsuccess = event => {
+        const cursor = event.target.result;
+        if(cursor) {
+          data.push(cursor.value);
+          cursor.continue();
+        }
+        else {
+          deferred.resolve(data);
         }
       }
-
-      dbPromise.onsuccess = event => {
-        const database = event.target.result;
-        const os = database.transaction('song', 'readwrite').objectStore('song');
-        const oc = os.index('band').openCursor(bandName);
-        const data = [];
-        
-        oc.onsuccess = event => {
-          const cursor = event.target.result;
-          if(cursor) {
-            data.push(cursor.value);
-            cursor.continue();
-          }
-          else {
-            resolve(data);
-          }
-        }
-        oc.onerror = () => {
-          reject("Error retrieving data");
-        }
-        
+      oc.onerror = () => {
+        defereeed.reject("Error retrieving data");
       }
+      
+    }
 
-      dbPromise.onerror = () => {
-        reject("Connecting to database failed");
-      }
-    })
+    return deferred.promise;
   }
 
   $database.getBandNames = () => {
-    return new Promise(function(resolve, reject) {
-      if(!indexedDB) {
-        reject("Your browser doesn't support IndexedDB");
-      }
+    const { deferred, dbPromise } = prepareDatabase();
 
-      const dbPromise = indexedDB.open(DB_NAME, DB_VERSION);
-
-      dbPromise.onupgradeneeded = event => {
-        const database = event.target.result;
-        if(!database.objectStoreNames.contains('song')) {
-          database
-            .createObjectStore('song', { keyPath: 'id', autoIncrement: true })
-            .createIndex('band', 'band', { unique: false })
+    dbPromise.onsuccess = event => {
+      const database = event.target.result;
+      const os = database.transaction('song', 'readwrite').objectStore('song');
+      const oc = os.index('band').openCursor(undefined, "nextunique");
+      const data = [];
+      
+      oc.onsuccess = event => {
+        const cursor = event.target.result;
+        if(cursor) {
+          data.push(cursor.value.band);
+          cursor.continue();
+        }
+        else {
+          deferred.resolve(data);
         }
       }
-
-      dbPromise.onsuccess = event => {
-        const database = event.target.result;
-        const os = database.transaction('song', 'readwrite').objectStore('song');
-        const oc = os.index('band').openCursor(undefined, "nextunique");
-        const data = [];
-        
-        oc.onsuccess = event => {
-          const cursor = event.target.result;
-          if(cursor) {
-            data.push(cursor.value.band);
-            cursor.continue();
-          }
-          else {
-            resolve(data);
-          }
-        }
-        oc.onerror = () => {
-          reject("Error retrieving data");
-        }
-        
+      oc.onerror = () => {
+        deferred.reject("Error retrieving data");
       }
+    }
 
-      dbPromise.onerror = () => {
-        reject("Connecting to database failed");
-      }
-    })
+    return deferred.promise;
   }
 
   return $database;
